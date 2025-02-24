4637a95844eafa62a9173e088befa841c7df623c
Izimov Erden <erden.izimov@gmail.com>
2025-02-25 00:56:45 +0500

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,36 @@
-$(document).ready(function(){
-    $('.carusel_inner').slick({
-     infinite: true,
-  speed: 300,
-    prevArrow:'<button type="button" class="slick-prev"><img src="../icons/left.png>"></button>',
-  nextArrow:'<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
-  responsive: [ {
-    breakpoint: 768,
-    settings: {
-            dots: true,
-            arrows:false,
-    }
-  }
-  ]
+//  $(document).ready(function(){
+//     $('.carusel_inner').slick({
+//      infinite: true,
+//   speed: 300,
+//     prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.png>"></button>',
+//     nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+//   responsive: [ {
+//     breakpoint: 768,
+//     settings: {
+//             dots: true,
+//             arrows:false,
+//     }
+//   }
+//   ]
 
   
-    });
-  });
-          
+//     });
+//   });
+
+
+
+var slider = tns({
+  container: '.carusel_inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls:false,
+  nav:false
+});
+
+document.querySelector('.prev').addEventListener('click', function  (){
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function  (){
+  slider.goTo('next');
+});
